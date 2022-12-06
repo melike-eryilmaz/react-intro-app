9ca5789a2dbd37978ae70501617d64d38afddf71
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2022-12-06 14:05:38 +0300

    json-server added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import React, { Component } from "react";
 import CategoryList from "./CategoryList";
 import Navigation from "./Navigation";
 import ProductList from "./ProductList";
 import { Container,Row ,Col} from "reactstrap";
-function App() {
+
+class App extends Component {
+  /**
+   * React da tek yönlü data aktarımı olduğu için değişen kategoriyi productlist componentede aktarmak için
+   * changecategory func 'i buraya taşıdık ve bunu yapabilmek için functional yapıdan class yapıya çevirdik.
+   */
+  state = {
+    currentCategory:""
+  };
+  changeCategory = (category)=>{
+    this.setState({currentCategory:category.categoryName})
+  }
+  render() {
+    let productInfo = {title:"Product List"};
+    let categoryInfo = {title:"Category List",content:"Category content"};
+    let navigationInfo = {title:"Navigation Component"};
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navigation info={navigationInfo}></Navigation>
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList info={categoryInfo} changeCategory={this.changeCategory} currentCategory={this.state.currentCategory}></CategoryList>
+            </Col>
+            <Col xs ="9">
+            <ProductList info={productInfo} currentCategory={this.state.currentCategory}></ProductList>
+            </Col>
+          </Row>
+        </Container>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
+/**
+ * 
+ * function App() {
   let productInfo = {title:"Product List"};
   let categoryInfo = {title:"Category List",content:"Category content"};
   let navigationInfo = {title:"Navigation Component"};
@@ -27,3 +68,6 @@ function App() {
 }
 
 export default App;
+ * @returns 
+ */
+
--- src/ProductList.js
+++ src/ProductList.js
@@ -4,7 +4,7 @@ class ProductList extends Component {
     render() {
         return (
             <div>
-               <h1> {this.props.info.title}</h1>
+               <h1> {this.props.info.title}-{this.props.currentCategory}</h1>
             </div>
         );
     }
