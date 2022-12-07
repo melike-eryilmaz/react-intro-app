a4bbac484fb13571371dc85109fbe9070b080397
melike-eryilmaz <41503266+melike-eryilmaz@users.noreply.github.com>
2022-12-07 11:04:39 +0300

    Revert "json-server added"
    This reverts commit 9ca5789a2dbd37978ae70501617d64d38afddf71.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
-import React, { Component } from "react";
 import CategoryList from "./CategoryList";
 import Navigation from "./Navigation";
 import ProductList from "./ProductList";
 import { Container,Row ,Col} from "reactstrap";
-
-class App extends Component {
-  /**
-   * React da tek yönlü data aktarımı olduğu için değişen kategoriyi productlist componentede aktarmak için
-   * changecategory func 'i buraya taşıdık ve bunu yapabilmek için functional yapıdan class yapıya çevirdik.
-   */
-  state = {
-    currentCategory:""
-  };
-  changeCategory = (category)=>{
-    this.setState({currentCategory:category.categoryName})
-  }
-  render() {
-    let productInfo = {title:"Product List"};
-    let categoryInfo = {title:"Category List",content:"Category content"};
-    let navigationInfo = {title:"Navigation Component"};
-    return (
-      <div>
-        <Container>
-          <Row>
-            <Navigation info={navigationInfo}></Navigation>
-          </Row>
-          <Row>
-            <Col xs="3">
-              <CategoryList info={categoryInfo} changeCategory={this.changeCategory} currentCategory={this.state.currentCategory}></CategoryList>
-            </Col>
-            <Col xs ="9">
-            <ProductList info={productInfo} currentCategory={this.state.currentCategory}></ProductList>
-            </Col>
-          </Row>
-        </Container>
-        
-      </div>
-    );
-  }
-}
-
-export default App;
-/**
- * 
- * function App() {
+function App() {
   let productInfo = {title:"Product List"};
   let categoryInfo = {title:"Category List",content:"Category content"};
   let navigationInfo = {title:"Navigation Component"};
@@ -68,6 +27,3 @@ export default App;
 }
 
 export default App;
- * @returns 
- */
-
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,4 +1,3 @@
-import { response } from "express";
 import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem } from "reactstrap";
 class CategoryList extends Component {
@@ -11,27 +10,14 @@ class CategoryList extends Component {
       { categorId: 1, categoryName: "Beverages" },
       { categorId: 2, categoryName: "Foods" },
     ],
-   // currentCategory:""
   };
-  // changeCategory = (category)=>{
-  //   this.setState({currentCategory:category.categoryName})
-  // }
-
-  // componentDidMount(){
-  //   this.getCategories();
-  // }
-  // getCategories = ()=>{
-  //   fetch("http://localhost:3000/categories")
-  //   .then(response=>response.json())
-  //   .then(data =>this.setState({categories:data}));
-  // }
   render() {
     return (
       <Container>
         <h1>{this.props.info.title}</h1>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem onClick={()=>this.props.changeCategory(category)} key={category.id}>
+            <ListGroupItem key={category.categorId}>
               {category.categoryName}
             </ListGroupItem>
           ))}
@@ -43,7 +29,6 @@ class CategoryList extends Component {
             <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
             <ListGroupItem>Vestibulum at eros</ListGroupItem> */}
         </ListGroup>
-        <h4>{this.props.currentCategory}</h4>
       </Container>
     );
   }
